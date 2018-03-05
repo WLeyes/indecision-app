17dd2a9205ce62ffb6465a0ebdcdbe8caa9ca4a8
Warren Leyes <warren.leyes@gmail.com>
2018-03-05 01:09:17 -0800

    created first basic templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,74 @@
 'use strict';
 
 console.log('App.js is running');
-
 // JSX - Javascript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'Looking for change'
-);
-var appRoot = document.querySelector('.app');
 
+
+////////////////////////////////////////////////////////////////////////////////
+// TEMPLATE 1
+////////////////////////////////////////////////////////////////////////////////
+
+var mainHeading = {
+  title: 'Indecision App.',
+  subTitle: 'Put your life in the hands of a computer.'
+};
+
+var template = React.createElement(
+  'article',
+  null,
+  React.createElement(
+    'section',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      mainHeading.title
+    ),
+    React.createElement(
+      'h2',
+      null,
+      mainHeading.subTitle
+    )
+  )
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// TEMPLATE 2
+////////////////////////////////////////////////////////////////////////////////
+var user = {
+  firstName: 'John',
+  lastName: 'Smith',
+  age: 65,
+  location: 'Springfield'
+};
+
+var templateTwo = React.createElement(
+  'article',
+  null,
+  React.createElement(
+    'section',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      user.firstName,
+      ' ',
+      user.lastName
+    ),
+    React.createElement(
+      'p',
+      null,
+      'Age: ',
+      user.age
+    ),
+    React.createElement(
+      'p',
+      null,
+      'Location: ',
+      user.location
+    )
+  )
+);
+
+var appRoot = document.querySelector('.app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 console.log('App.js is running');
-
 // JSX - Javascript XML
-var template = <h1>Looking for change</h1>;
-var appRoot = document.querySelector('.app');
 
+
+////////////////////////////////////////////////////////////////////////////////
+// TEMPLATE 1
+////////////////////////////////////////////////////////////////////////////////
+
+var mainHeading = {
+  title: 'Indecision App.',
+  subTitle: 'Put your life in the hands of a computer.'
+}
+
+var template = (
+  <article>
+    <section>
+      <h1>{mainHeading.title}</h1>
+      <h2>{mainHeading.subTitle}</h2>
+    </section>
+  </article>
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// TEMPLATE 2
+////////////////////////////////////////////////////////////////////////////////
+var user = {
+  firstName: 'John',
+  lastName : 'Smith',
+  age      :  65,
+  location: 'Springfield'
+};
+
+var templateTwo = (
+  <article>
+    <section>
+      <h1>{user.firstName} {user.lastName}</h1>
+      <p>Age: {user.age}</p>
+      <p>Location: {user.location}</p>
+    </section>
+  </article>
+);
+
+var appRoot = document.querySelector('.app');
 ReactDOM.render(template, appRoot);
